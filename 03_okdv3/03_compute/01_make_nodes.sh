06b5ea255560092674b2991240183bc6f20084b6
veldrane <gveldrane@gmail.com>
2019-12-19 09:27:30 +0100

    Added scripts for vm host, centos template,ipa and okd3

--- 03_okdv3/03_compute/01_make_nodes.sh
+++ 03_okdv3/03_compute/01_make_nodes.sh
@@ -3,11 +3,11 @@ i=1
 while [ "$i" -ne 5 ]
 do
     echo "node2$i.lab.local"
-    mkdir /vms/node2$i.lab.local
-    virt-clone --original-xml /vms/templates/centos-7.6/centos-7.6.xml --name node2$i.lab.local --file /vms/node2$i.lab.local/rootvg.qcow2
+    mkdir /data/vms/node2$i.lab.local
+    virt-clone --original-xml /data/templates/t_centos7/t_centos7.xml --name node2$i.lab.local --file /data/vms/node2$i.lab.local/rootvg.qcow2
     cat include/_setup_vars.template | sed s/XXX/$i/g > include/_setup_vars.yaml
     virsh setmaxmem node2$i.lab.local 8G --config
-    qemu-img resize /vms/node2$i.lab.local/rootvg.qcow2 +20G
+    qemu-img resize /data/vms/node2$i.lab.local/rootvg.qcow2 +20G
     ansible-playbook ./01_prepare_nodes.yaml    
     i=$((i + 1))
 done

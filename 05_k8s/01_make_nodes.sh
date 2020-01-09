78541d6b9fc03e670b44dd41d7887eef1755efbd
veldrane <gveldrane@gmail.com>
2020-01-10 00:15:15 +0100

    05_k8s is almost done - some fucking issues with ansible must be solved

--- 05_k8s/01_make_nodes.sh
+++ 05_k8s/01_make_nodes.sh
@@ -7,21 +7,28 @@
 #do
 #    echo "node$i.lab.local"
 #    cat ansible/include/_setup_vars.template | sed s/XXX/$i/g > ansible/include/_setup_vars.yaml
-#    cd ansible
-#    ansible-playbook ./01_prepare_nodes.yaml
+#    ansible-playbook ansible/01_prepare_nodes.yaml
+#    if [ $? -eq 1 ]; then
+#    	exit 1
+#    fi
+#    ansible-playbook ansible/02_install_master.yaml
 #    i=$((i + 1))
 #done
 
-#cd ../
+#sleep 60
 
 echo "Creating nodes..."
 
-i=81
-while [ "$i" -ne 85 ]
+i=86
+while [ "$i" -ne 89 ]
 do
     echo "node$i.lab.local"
+    arp -da 10.1.16.200
     cat ansible/include/_setup_vars.template | sed s/XXX/$i/g > ansible/include/_setup_vars.yaml
-    cd ansible
-    ansible-playbook ./01_prepare_nodes.yaml
+    ansible-playbook ansible/01_prepare_nodes.yaml
+    if [ $? -eq 1 ]; then
+       exit 1
+    fi
+    ansible-playbook ansible/03_install_nodes.yaml
     i=$((i + 1))
 done

26183cd5d1e75ecd85aab5fbede104a9727ad936
veldrane <gveldrane@gmail.com>
2021-04-06 12:41:39 +0200

    Add join node script

--- 05_k8s/01_join_workers.sh
+++ 05_k8s/01_join_workers.sh
@@ -1,6 +1,6 @@
 #!/bin/sh
 
-echo "Creating nodes..."
+echo "Joining nodes to existing cluster..."
 
 i=23
 while [ "$i" -ne 25 ]
@@ -10,10 +10,10 @@ do
     rm -rf /root/.ansible/cp/*
     rm -rf /root/.ansible/tmp/*
     cat ansible/include/_setup_vars.template | sed s/XXX/$i/g > ansible/include/_setup_vars.yaml
-    ansible-playbook ansible/01_prepare_nodes.yaml
-    if [ $? -eq 1 ]; then
-       exit 1
-    fi
+#    ansible-playbook ansible/01_prepare_nodes.yaml
+#    if [ $? -eq 1 ]; then
+#       exit 1
+#    fi
     ansible-playbook ansible/03_install_nodes.yaml
     i=$((i + 1))
 done
